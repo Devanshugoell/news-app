65ebe6eae5d70a42bd6788639a4dbb0e5eb66692
Devanshu <devgoyal952@gmail.com>
2024-08-26 12:19:31 +0530

    added Newsapp component

--- src/components/Newsapp.js
+++ src/components/Newsapp.js
@@ -1,8 +1,37 @@
-// import Card from "./Card";
+import Card from "./Card";
 import React from "react";
+import SearchIcon from "./search.png";
+import "./Newsapp.css";
 
 const Newsapp = () => {
-  return <div></div>;
+  return (
+    <div>
+      <nav>
+        <div>
+          <h1>TRENDY NEWS</h1>
+        </div>
+        <ul>
+          <h1>ALL TREANDING NEWS</h1>
+        </ul>
+        <div className="searchBar">
+          <input type="text" placeholder="Search News" />
+          <button>
+            <img src={SearchIcon} alt="search icon" />
+          </button>
+        </div>
+      </nav>
+      <div className="categoryBtn">
+        <button>Sports</button>
+        <button>Politics</button>
+        <button>Entertainment</button>
+        <button>Health</button>
+        <button>Fitness</button>
+      </div>
+      <div className="cardComponets">
+        <Card />
+      </div>
+    </div>
+  );
 };
 
 export default Newsapp;
