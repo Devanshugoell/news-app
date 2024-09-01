7d20ed7296b493e3c2b2ab81836973b5ff0ff951
Devanshu <devgoyal952@gmail.com>
2024-09-01 08:44:28 +0530

    mapped the different items

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,22 @@ import React from "react";
 
 const Card = ({ data }) => {
   console.log(data);
-  return <div className="cardContainer">Card</div>;
+  return (
+    <div className="cardContainer">
+      {data.map((curItem, index) => {
+        return (
+          <div className="card">
+            <img src={curItem.urlToImage} />
+            <div className="cardContent">
+              <a href="/">{curItem.title}</a>
+              <p>{curItem.description}</p>
+              <button>Read More</button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Card;
